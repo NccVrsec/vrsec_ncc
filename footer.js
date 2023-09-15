0b8f15a01a8058de930dd791d6cf226592fed35e
Yamuna <ampalamyamuna2002@gmail.com>
2023-09-15 19:55:39 +0530

    first commit

---
+++ footer.js
@@ -0,0 +1,10 @@
+var typed3 = new Typed('#typed3', {
+    strings: ['by Mahith  (208W1A1282)', 'by Rizwan (208W1A1299)', 'by Monica (208W1A12A5)', 'for VRSEC NCC'],
+    typeSpeed: 90,
+    backSpeed: 0,
+    // backDelay: 500,
+    // startDelay: 1000,
+    smartBackspace: true, // this is a default
+    loop: true,
+    showCursor: false
+});
